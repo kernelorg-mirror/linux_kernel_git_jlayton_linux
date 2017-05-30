ac7167f64032147e38fd9b2f1fd22df8d69c9c8f
Jeff Layton <jlayton@redhat.com>
2017-05-30 09:52:22 -0400

    fs: remove unneeded forward definition of mm_struct from fs.h
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Reviewed-by: Jan Kara <jack@suse.cz>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1254,8 +1254,6 @@ extern void f_delown(struct file *filp);
 extern pid_t f_getown(struct file *filp);
 extern int send_sigurg(struct fown_struct *fown);
 
-struct mm_struct;
-
 /*
  *	Umount options
  */
