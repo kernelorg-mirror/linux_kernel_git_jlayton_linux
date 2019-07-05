a435f5eb8f5d0b000e0f6e2b9e9f219c8565041e
Jeff Layton <jlayton@kernel.org>
2019-07-05 13:26:29 -0400

    ceph: remove unneeded test in try_flush_caps
    If cap->session is always non-NULL, so we can just do a single test for
    equality w/o testing explicitly for a NULL pointer.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>

--- fs/ceph/caps.c
+++ fs/ceph/caps.c
@@ -2122,7 +2122,7 @@ retry_locked:
 		struct ceph_cap *cap = ci->i_auth_cap;
 		int delayed;
 
-		if (!session || session != cap->session) {
+		if (session != cap->session) {
 			spin_unlock(&ci->i_ceph_lock);
 			if (session)
 				mutex_unlock(&session->s_mutex);
