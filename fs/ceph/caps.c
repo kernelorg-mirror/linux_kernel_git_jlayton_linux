388806f8c386c338cb111c761be5a57fe547078d
Jeff Layton <jlayton@kernel.org>
2019-07-22 13:12:01 -0400

    ceph: fetch cap_gen under spinlock in ceph_add_cap
    It's protected by the s_gen_ttl_lock, so we should fetch under it
    and ensure that we're using the same generation in both places.
    
    Signed-off-by: Jeff Layton <jlayton@kernel.org>

--- fs/ceph/caps.c
+++ fs/ceph/caps.c
@@ -641,6 +641,7 @@ void ceph_add_cap(struct inode *inode,
 	struct ceph_cap *cap;
 	int mds = session->s_mds;
 	int actual_wanted;
+	u32 gen;
 
 	lockdep_assert_held(&ci->i_ceph_lock);
 
@@ -653,6 +654,10 @@ void ceph_add_cap(struct inode *inode,
 	 */
 	if (fmode >= 0)
 		wanted |= ceph_caps_for_mode(fmode);
+
+	spin_lock(&session->s_gen_ttl_lock);
+	gen = session->s_cap_gen;
+	spin_unlock(&session->s_gen_ttl_lock);
 
 	cap = __get_cap_for_mds(ci, mds);
 	if (!cap) {
@@ -679,7 +684,7 @@ void ceph_add_cap(struct inode *inode,
 		list_move_tail(&cap->session_caps, &session->s_caps);
 		spin_unlock(&session->s_cap_lock);
 
-		if (cap->cap_gen < session->s_cap_gen)
+		if (cap->cap_gen < gen)
 			cap->issued = cap->implemented = CEPH_CAP_PIN;
 
 		/*
@@ -773,7 +778,7 @@ void ceph_add_cap(struct inode *inode,
 	cap->seq = seq;
 	cap->issue_seq = seq;
 	cap->mseq = mseq;
-	cap->cap_gen = session->s_cap_gen;
+	cap->cap_gen = gen;
 
 	if (fmode >= 0)
 		__ceph_get_fmode(ci, fmode);
