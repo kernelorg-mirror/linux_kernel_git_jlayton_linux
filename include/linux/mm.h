0fa1c7f36cdc9a0ceacfcf397cb00c777ea51896
David Howells <dhowells@redhat.com>
2020-05-23 17:15:49 +0100

    mm: Provide lru_to_last_page() to get last of a page list
    Provide a macro, lru_to_last_page(), to find the last page in a page list
    (the opposite of lru_to_page()).
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -220,6 +220,7 @@ int overcommit_policy_handler(struct ctl_table *, int, void *, size_t *,
 #define PAGE_ALIGNED(addr)	IS_ALIGNED((unsigned long)(addr), PAGE_SIZE)
 
 #define lru_to_page(head) (list_entry((head)->prev, struct page, lru))
+#define lru_to_last_page(head) (list_entry((head)->next, struct page, lru))
 
 /*
  * Linux kernel virtual memory manager primitives.
