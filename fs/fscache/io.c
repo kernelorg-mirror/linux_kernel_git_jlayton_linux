17459cfc711ca2a308c3bc8f47f84ff751d8c79e
David Howells <dhowells@redhat.com>
2021-09-14 16:46:09 +0100

    fscache: Disable fscache_begin_operation()
    Disable fscache_begin_operation() so that the operation manager can be
    removed and replaced.
    
    Signed-off-by: David Howells <dhowells@redhat.com>

--- fs/fscache/io.c
+++ fs/fscache/io.c
@@ -30,6 +30,7 @@ int __fscache_begin_operation(struct netfs_cache_resources *cres,
 			      struct fscache_cookie *cookie,
 			      bool for_write)
 {
+#if 0
 	struct fscache_operation *op;
 	struct fscache_object *object;
 	bool wake_cookie = false;
@@ -143,6 +144,7 @@ nobufs:
 		fscache_stat(&fscache_n_stores_nobufs);
 	else
 		fscache_stat(&fscache_n_retrievals_nobufs);
+#endif
 	_leave(" = -ENOBUFS");
 	return -ENOBUFS;
 }
