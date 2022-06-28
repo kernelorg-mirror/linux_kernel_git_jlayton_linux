ammarfaizi2/linux-block

netfs: Do copy-to-cache-on-read through VM writeback
When data is read from the server and intended to be copied to the cache,
offload the cache write to the VM writeback mechanism rather than
scheduling it immediately.  This allows the downloaded data to be
superseded by local changes before it is written to the cache and means
that we no longer need to use the PG_fscache flag.

This is done by the following means:

 (1) The pages just downloaded into are marked dirty in
     netfs_rreq_unlock().

 (2) A region of NETFS_REGION_CACHE_COPY type is added to the dirty region
     list.

 (3) If a region-to-be-modified overlaps the cache-copy region, the
     modifications supersede the download, moving the end marker over in
     netfs_merge_dirty_region().

 (4) We don't really want to supersede in the middle of a region, so we may
     split a pristine region so that we can supersede forwards only.

 (5) We mark regions we're going to supersede with NETFS_REGION_SUPERSEDED
     to prevent them getting merged whilst we're superseding them.  This
     flag is cleared when we're done and we may merge afterwards.

 (6) Adjacent download regions are potentially mergeable.

 (7) When being flushed, CACHE_COPY regions are intended only to be written
     to the cache, not the server, though they may contribute data to a
     cross-page chunk that has to be encrypted or compressed and sent to
     the server.

Signed-off-by: David Howells <dhowells@redhat.com>

fs/netfs/internal.h
@@ -98,6 +98,7 @@ void netfs_writeback_worker(struct work_struct *work);
 void netfs_flush_region(struct netfs_i_context *ctx,
 			struct netfs_dirty_region *region,
 			enum netfs_dirty_trace why);
+void netfs_rreq_do_write_to_cache(struct netfs_read_request *rreq);
 
 /*
  * write_prep.c
@@ -121,10 +122,7 @@ extern atomic_t netfs_n_rh_read_done;
 extern atomic_t netfs_n_rh_read_failed;
 extern atomic_t netfs_n_rh_zero;
 extern atomic_t netfs_n_rh_short_read;
-extern atomic_t netfs_n_rh_write;
 extern atomic_t netfs_n_rh_write_begin;
-extern atomic_t netfs_n_rh_write_done;
-extern atomic_t netfs_n_rh_write_failed;
 extern atomic_t netfs_n_rh_write_zskip;
 extern atomic_t netfs_n_wh_region;
 extern atomic_t netfs_n_wh_flush_group;

kernelorg-mirror/linux_kernel_git_jlayton_linux

 (1) The just-read pages are marked dirty by netfs_rreq_unlock().
 (2) An entry of value NETFS_REGION_CACHE_COPY is added to the dirty region
     list covering the affected range.
 (3) Adjacent CACHE_COPY entries are merged together.
 (4) If a dirty region is added to the list that overlaps with CACHE_COPY
     marked pages, if just displaces those from the tree.
 (5) When being flushed, CACHE_COPY regions are intended only to be written
     multi-page chunk that has to be encrypted or compressed and sent to

fs/netfs/internal.h
@@ -40,6 +40,7 @@ bool netfs_are_regions_mergeable(struct netfs_inode *ctx,
 				 const struct netfs_dirty_region *b);
 struct netfs_dirty_region *netfs_find_region(struct netfs_inode *ctx,
 					     pgoff_t first, pgoff_t last);
+void netfs_rreq_do_write_to_cache(struct netfs_io_request *rreq);
 
 /*
  * crypto.c
@@ -61,6 +62,7 @@ ssize_t netfs_direct_write_iter(struct kiocb *iocb, struct iov_iter *from);
 /*
  * io.c
  */
+void netfs_rreq_completed(struct netfs_io_request *rreq, bool was_async);
 ssize_t netfs_begin_read(struct netfs_io_request *rreq, bool sync);
 
 /*
@@ -182,10 +184,7 @@ extern atomic_t netfs_n_rh_read_done;
 extern atomic_t netfs_n_rh_read_failed;
 extern atomic_t netfs_n_rh_zero;
 extern atomic_t netfs_n_rh_short_read;
-extern atomic_t netfs_n_rh_write;
 extern atomic_t netfs_n_rh_write_begin;
-extern atomic_t netfs_n_rh_write_done;
-extern atomic_t netfs_n_rh_write_failed;
 extern atomic_t netfs_n_rh_write_zskip;
 extern atomic_t netfs_n_wh_region;
 extern atomic_t netfs_n_wh_upload;