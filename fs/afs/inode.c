ammarfaizi2/linux-block

netfs: Track the fpos above which the server has no data
Track the file position above which the server is not expected to have any
data and preemptively assume that we can simply fill blocks with zeroes
locally rather than attempting to download them - even if we've written
data back to the server.  Assume that any data that was written back above
that position is held in the local cache.  Call this the "zero point".

Make use of this to optimise away some reads from the server.  We need to
set the zero point in the following circumstances:

 (1) When we see an extant remote inode and have no cache for it, we set
     the zero_point to i_size.

 (2) On local inode creation, we set zero_point to 0.

 (3) On local truncation down, we reduce zero_point to the new i_size if
     the new i_size is lower.

 (4) On local truncation up, we don't change zero_point.

 (5) On local modification, we don't change zero_point.

 (6) On remote invalidation, we set zero_point to the new i_size.

 (7) If stored data is culled from the local cache, we must set zero_point
     above that if the data also got written to the server.

 (8) If dirty data is written back to the server, but not the local cache,
     we must set zero_point above that.

Assuming the above, any read from the server at or above the zero_point
position will return all zeroes.

The zero_point value can be stored in the cache, provided the above rules
are applied to it by any code that culls part of the local cache.

Signed-off-by: David Howells <dhowells@redhat.com>

fs/afs/inode.c
@@ -253,6 +253,7 @@ static void afs_apply_status(struct afs_operation *op,
 		 */
 		if (change_size) {
 			afs_set_i_size(vnode, status->size);
+			vnode->netfs_ctx.zero_point = status->size;
 			inode->i_ctime = t;
 			inode->i_atime = t;
 		}
@@ -841,11 +842,11 @@ static void afs_setattr_edit_file(struct afs_operation *op)
 		loff_t size = op->setattr.attr->ia_size;
 		loff_t i_size = op->setattr.old_i_size;
 
-		if (size < i_size)
+		if (size != i_size) {
 			truncate_pagecache(inode, size);
-		if (size != i_size)
-			fscache_resize_cookie(afs_vnode_cache(vp->vnode),
-					      vp->scb.status.size);
+			netfs_resize_file(inode, size);
+			fscache_resize_cookie(afs_vnode_cache(vp->vnode), size);
+		}
 	}
 }
 

kernelorg-mirror/linux_kernel_git_jlayton_linux

fs/afs/inode.c
@@ -249,6 +249,7 @@ static void afs_apply_status(struct afs_operation *op,
 		vnode->netfs.remote_i_size = status->size;
 		if (change_size) {
 			afs_set_i_size(vnode, status->size);
+			vnode->netfs.zero_point = status->size;
 			inode->i_ctime = t;
 			inode->i_atime = t;
 		}
@@ -850,11 +851,11 @@ static void afs_setattr_edit_file(struct afs_operation *op)
 		loff_t size = op->setattr.attr->ia_size;
 		loff_t i_size = op->setattr.old_i_size;
 
-		if (size < i_size)
+		if (size != i_size) {
 			truncate_pagecache(inode, size);
-		if (size != i_size)
-			fscache_resize_cookie(afs_vnode_cache(vp->vnode),
-					      vp->scb.status.size);
+			netfs_resize_file(inode, size);
+			fscache_resize_cookie(afs_vnode_cache(vp->vnode), size);
+		}
 	}
 }
 