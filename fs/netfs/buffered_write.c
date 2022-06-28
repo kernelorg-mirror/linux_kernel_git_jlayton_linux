ammarfaizi2/linux-block

netfs: Check for conflicting regions when about to perform a write
Check for conflicting dirty regions when we're about to perform a new
write.  We might have dirty data with different authentication, for
example, or that needs to be written back in the right order (ceph snaps,
for example).

Signed-off-by: David Howells <dhowells@redhat.com>

fs/netfs/buffered_write.c
@@ -682,6 +682,7 @@ ssize_t netfs_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 {
 	struct file *file = iocb->ki_filp;
 	struct inode *inode = file->f_mapping->host;
+	struct netfs_i_context *ctx = netfs_i_context(inode);
 	ssize_t ret;
 
 	_enter("%llx,%zx,%llx", iocb->ki_pos, iov_iter_count(from), i_size_read(inode));
@@ -701,6 +702,11 @@ ssize_t netfs_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 
 	ret = file_update_time(file);
 	if (ret)
+		goto error;
+
+	ret = netfs_flush_conflicting_writes(ctx, file, iocb->ki_pos,
+					     iov_iter_count(from), NULL);
+	if (ret < 0 && ret != -EAGAIN)
 		goto error;
 
 	if (iocb->ki_flags & IOCB_DIRECT)

fs/netfs/internal.h
@@ -36,6 +36,12 @@ static inline bool netfs_mas_is_valid(const void *mas_entry)
 }
 
 /*
+ * buffered_flush.c
+ */
+int netfs_flush_conflicting_writes(struct netfs_i_context *ctx, struct file *file,
+				   loff_t start, size_t len, struct folio *unlock_this);
+
+/*
  * buffered_read.c
  */
 void netfs_rreq_unlock_folios(struct netfs_io_request *rreq);

kernelorg-mirror/linux_kernel_git_jlayton_linux

fs/netfs/buffered_write.c
@@ -576,6 +576,7 @@ ssize_t netfs_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 {
 	struct file *file = iocb->ki_filp;
 	struct inode *inode = file->f_mapping->host;
+	struct netfs_inode *ctx = netfs_inode(inode);
 	ssize_t ret;
 
 	_enter("%llx,%zx,%llx", iocb->ki_pos, iov_iter_count(from), i_size_read(inode));
@@ -595,6 +596,11 @@ ssize_t netfs_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 
 	ret = file_update_time(file);
 	if (ret)
+		goto error;
+
+	ret = netfs_flush_conflicting_writes(ctx, file, iocb->ki_pos,
+					     iov_iter_count(from), NULL);
+	if (ret < 0 && ret != -EAGAIN)
 		goto error;
 
 	if (iocb->ki_flags & IOCB_DIRECT)

fs/netfs/internal.h
@@ -16,6 +16,12 @@
 #define pr_fmt(fmt) "netfs: " fmt
 
 /*
+ * buffered_flush.c
+ */
+int netfs_flush_conflicting_writes(struct netfs_inode *ctx, struct file *file,
+				   loff_t start, size_t len, struct folio *unlock_this);
+
+/*
  * buffered_read.c
  */
 void netfs_rreq_unlock_folios(struct netfs_io_request *rreq);