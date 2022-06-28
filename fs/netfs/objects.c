ammarfaizi2/linux-block

netfs: Define a struct to track dirty regions of a file
Define struct netfs_dirty_region to track the dirty regions of a file.  The
regions for each inode are kept in a maple tree in the inode context.  The
dirty region struct gives the filesystem somewhere to hang its data on a
per-region basis without using page->private.  Each dirty region has a
corresponding entry in the maple tree based on the first and last pages in
the region.

Data that has been read from the server and that simply needs to be written
to the cache will be marked in the maple tree with a special value, but
does not have a netfs_dirty_region directly associated with it.  This
allows spans of data that just needs copying to the cache to be split to
insert a new region without the need to allocate an extra region or extra
maple tree nodes.

The flusher will scan the dirty region tree looking for things to write out
and will dispatch them either just to the cache or the network and the
cache.

When a region is being written out, new regions will be snipped off to be
written out to the network and/or the cache, and the affected region in the
maple tree will be updated.  If a new change comes along whilst the write
is taking place, this will (partially) overwrite that part of the tree and
set a flag in the region to indicate that it's undergoing writeback.  The
flag will suppress further writeback from the same place.  The flags are
removed when the writeback completes.  If writeback fails, the region can
just be reinstated back into the maple tree, subsuming any overlayed
regions.

Regions are rounded out to the size of the constituent folios (which may be
larger than a page), crypto granulatity and cache granularity as
appropriate.

Signed-off-by: David Howells <dhowells@redhat.com>

fs/netfs/objects.c
@@ -8,6 +8,8 @@
 #include <linux/slab.h>
 #include "internal.h"
 
+atomic_t netfs_region_debug_ids;
+
 /*
  * Allocate an I/O request and initialise it.
  */
@@ -40,6 +42,7 @@ struct netfs_io_request *netfs_alloc_request(struct address_space *mapping,
 	xa_init(&rreq->buffer);
 	xa_init(&rreq->bounce);
 	INIT_LIST_HEAD(&rreq->subrequests);
+	INIT_LIST_HEAD(&rreq->regions);
 	refcount_set(&rreq->ref, 1);
 
 	__set_bit(NETFS_RREQ_IN_PROGRESS, &rreq->flags);
@@ -86,6 +89,18 @@ void netfs_clear_subrequests(struct netfs_io_request *rreq, bool was_async)
 	}
 }
 
+static void netfs_clear_regions(struct netfs_io_request *rreq)
+{
+	struct netfs_dirty_region *region;
+	struct netfs_i_context *ctx = netfs_i_context(rreq->inode);
+
+	while ((region = list_first_entry_or_null(
+			&rreq->regions, struct netfs_dirty_region, flush_link))) {
+		list_del_init(&region->flush_link);
+		netfs_put_dirty_region(ctx, region, netfs_region_trace_put_clear);
+	}
+}
+
 static void netfs_free_request(struct work_struct *work)
 {
 	struct netfs_io_request *rreq =
@@ -95,6 +110,8 @@ static void netfs_free_request(struct work_struct *work)
 	trace_netfs_rreq(rreq, netfs_rreq_trace_free);
 	netfs_proc_del_rreq(rreq);
 	netfs_clear_subrequests(rreq, false);
+	netfs_clear_regions(rreq);
+
 	if (rreq->netfs_ops->free_request)
 		rreq->netfs_ops->free_request(rreq);
 	if (rreq->cache_resources.ops)
@@ -188,4 +205,58 @@ void netfs_put_subrequest(struct netfs_io_subrequest *subreq, bool was_async,
 	trace_netfs_sreq_ref(debug_id, debug_index, r - 1, what);
 	if (dead)
 		netfs_free_subrequest(subreq, was_async);
+}
+
+/*
+ * Allocate a dirty region record.
+ */
+struct netfs_dirty_region *netfs_alloc_dirty_region(void)
+{
+	struct netfs_dirty_region *region;
+
+	region = kzalloc(sizeof(struct netfs_dirty_region), GFP_KERNEL);
+	if (region) {
+		refcount_set(&region->ref, 1);
+		INIT_LIST_HEAD(&region->flush_link);
+		netfs_stat(&netfs_n_wh_region);
+	}
+	return region;
+}
+
+struct netfs_dirty_region *netfs_get_dirty_region(struct netfs_i_context *ctx,
+						  struct netfs_dirty_region *region,
+						  enum netfs_region_trace what)
+{
+	int ref;
+
+	__refcount_inc(&region->ref, &ref);
+	trace_netfs_ref_region(region->debug_id, ref + 1, what);
+	return region;
+}
+
+void netfs_free_dirty_region(struct netfs_i_context *ctx,
+			     struct netfs_dirty_region *region)
+{
+	if (region) {
+		trace_netfs_ref_region(region->debug_id, 0, netfs_region_trace_free);
+		if (ctx->ops->free_dirty_region)
+			ctx->ops->free_dirty_region(region);
+		netfs_stat_d(&netfs_n_wh_region);
+		kfree(region);
+	}
+}
+
+void netfs_put_dirty_region(struct netfs_i_context *ctx,
+			    struct netfs_dirty_region *region,
+			    enum netfs_region_trace what)
+{
+	bool dead;
+	int ref;
+
+	if (!region)
+		return;
+	dead = __refcount_dec_and_test(&region->ref, &ref);
+	trace_netfs_ref_region(region->debug_id, ref - 1, what);
+	if (dead)
+		netfs_free_dirty_region(ctx, region);
 }

include/trace/events/netfs.h
@@ -105,6 +105,10 @@
 	EM(netfs_sreq_trace_put_work,		"PUT WORK   ")	\
 	E_(netfs_sreq_trace_put_terminated,	"PUT TERM   ")
 
+#define netfs_region_traces					\
+	EM(netfs_region_trace_free,		"FREE       ")	\
+	E_(netfs_region_trace_put_clear,	"PUT CLEAR  ")
+
 #ifndef __NETFS_DECLARE_TRACE_ENUMS_ONCE_ONLY
 #define __NETFS_DECLARE_TRACE_ENUMS_ONCE_ONLY
 
@@ -119,6 +123,7 @@ enum netfs_sreq_trace { netfs_sreq_traces } __mode(byte);
 enum netfs_failure { netfs_failures } __mode(byte);
 enum netfs_rreq_ref_trace { netfs_rreq_ref_traces } __mode(byte);
 enum netfs_sreq_ref_trace { netfs_sreq_ref_traces } __mode(byte);
+enum netfs_region_trace { netfs_region_traces } __mode(byte);
 
 #endif
 
@@ -138,6 +143,7 @@ netfs_sreq_traces;
 netfs_failures;
 netfs_rreq_ref_traces;
 netfs_sreq_ref_traces;
+netfs_region_traces;
 
 /*
  * Now redefine the EM() and E_() macros to map the enums to the strings that
@@ -355,9 +361,12 @@ TRACE_EVENT(netfs_wback,
 
 	    TP_fast_assign(
 		    struct fscache_cookie *__cookie = netfs_i_cookie(wreq->inode);
+		    struct netfs_dirty_region *__region =
+		    list_first_entry_or_null(&wreq->regions,
+					     struct netfs_dirty_region, flush_link);
 		    __entry->wreq	= wreq->debug_id;
 		    __entry->cookie	= __cookie ? __cookie->debug_id : 0;
-		    __entry->region	= 0;
+		    __entry->region	= __region ? __region->debug_id : 0;
 		    __entry->start	= wreq->start;
 		    __entry->len	= wreq->len;
 		    __entry->first	= wreq->first;
@@ -370,6 +379,30 @@ TRACE_EVENT(netfs_wback,
 		      __entry->region,
 		      __entry->start, __entry->start + __entry->len - 1,
 		      __entry->first, __entry->last)
+	    );
+
+TRACE_EVENT(netfs_ref_region,
+	    TP_PROTO(unsigned int region_debug_id, int ref,
+		     enum netfs_region_trace what),
+
+	    TP_ARGS(region_debug_id, ref, what),
+
+	    TP_STRUCT__entry(
+		    __field(unsigned int,		region		)
+		    __field(int,			ref		)
+		    __field(enum netfs_region_trace,	what		)
+			     ),
+
+	    TP_fast_assign(
+		    __entry->region	= region_debug_id;
+		    __entry->ref	= ref;
+		    __entry->what	= what;
+			   ),
+
+	    TP_printk("D=%x %s r=%u",
+		      __entry->region,
+		      __print_symbolic(__entry->what, netfs_region_traces),
+		      __entry->ref)
 	    );
 
 #undef EM

kernelorg-mirror/linux_kernel_git_jlayton_linux

fs/netfs/objects.c
@@ -8,6 +8,8 @@
 #include <linux/slab.h>
 #include "internal.h"
 
+atomic_t netfs_region_debug_ids;
+
 /*
  * Allocate an I/O request and initialise it.
  */
@@ -40,6 +42,7 @@ struct netfs_io_request *netfs_alloc_request(struct address_space *mapping,
 	xa_init(&rreq->buffer);
 	xa_init(&rreq->bounce);
 	INIT_LIST_HEAD(&rreq->subrequests);
+	INIT_LIST_HEAD(&rreq->regions);
 	refcount_set(&rreq->ref, 1);
 
 	__set_bit(NETFS_RREQ_IN_PROGRESS, &rreq->flags);
@@ -86,6 +89,18 @@ void netfs_clear_subrequests(struct netfs_io_request *rreq, bool was_async)
 	}
 }
 
+static void netfs_clear_regions(struct netfs_io_request *rreq)
+{
+	struct netfs_dirty_region *region;
+	struct netfs_inode *ctx = netfs_inode(rreq->inode);
+
+	while ((region = list_first_entry_or_null(
+			&rreq->regions, struct netfs_dirty_region, dirty_link))) {
+		list_del_init(&region->dirty_link);
+		netfs_put_dirty_region(ctx, region, netfs_region_trace_put_clear);
+	}
+}
+
 static void netfs_free_request(struct work_struct *work)
 {
 	struct netfs_io_request *rreq =
@@ -95,6 +110,8 @@ static void netfs_free_request(struct work_struct *work)
 	trace_netfs_rreq(rreq, netfs_rreq_trace_free);
 	netfs_proc_del_rreq(rreq);
 	netfs_clear_subrequests(rreq, false);
+	netfs_clear_regions(rreq);
+
 	if (rreq->netfs_ops->free_request)
 		rreq->netfs_ops->free_request(rreq);
 	if (rreq->cache_resources.ops)
@@ -190,3 +207,57 @@ void netfs_put_subrequest(struct netfs_io_subrequest *subreq, bool was_async,
 		netfs_free_subrequest(subreq, was_async);
 }
 EXPORT_SYMBOL(netfs_put_subrequest);
+
+/*
+ * Allocate a dirty region record.
+ */
+struct netfs_dirty_region *netfs_alloc_dirty_region(void)
+{
+	struct netfs_dirty_region *region;
+
+	region = kzalloc(sizeof(struct netfs_dirty_region), GFP_KERNEL);
+	if (region) {
+		refcount_set(&region->ref, 1);
+		INIT_LIST_HEAD(&region->dirty_link);
+		netfs_stat(&netfs_n_wh_region);
+	}
+	return region;
+}
+
+struct netfs_dirty_region *netfs_get_dirty_region(struct netfs_inode *ctx,
+						  struct netfs_dirty_region *region,
+						  enum netfs_region_trace what)
+{
+	int ref;
+
+	__refcount_inc(&region->ref, &ref);
+	trace_netfs_ref_region(region->debug_id, ref + 1, what);
+	return region;
+}
+
+void netfs_free_dirty_region(struct netfs_inode *ctx,
+			     struct netfs_dirty_region *region)
+{
+	if (region) {
+		trace_netfs_ref_region(region->debug_id, 0, netfs_region_trace_free);
+		if (ctx->ops->free_dirty_region)
+			ctx->ops->free_dirty_region(region);
+		netfs_stat_d(&netfs_n_wh_region);
+		kfree(region);
+	}
+}
+
+void netfs_put_dirty_region(struct netfs_inode *ctx,
+			    struct netfs_dirty_region *region,
+			    enum netfs_region_trace what)
+{
+	bool dead;
+	int ref;
+
+	if (!region)
+		return;
+	dead = __refcount_dec_and_test(&region->ref, &ref);
+	trace_netfs_ref_region(region->debug_id, ref - 1, what);
+	if (dead)
+		netfs_free_dirty_region(ctx, region);
+}

include/trace/events/netfs.h
@@ -107,6 +107,10 @@
 	EM(netfs_sreq_trace_put_work,		"PUT WORK   ")	\
 	E_(netfs_sreq_trace_put_terminated,	"PUT TERM   ")
 
+#define netfs_region_traces					\
+	EM(netfs_region_trace_free,		"FREE       ")	\
+	E_(netfs_region_trace_put_clear,	"PUT CLEAR  ")
+
 #ifndef __NETFS_DECLARE_TRACE_ENUMS_ONCE_ONLY
 #define __NETFS_DECLARE_TRACE_ENUMS_ONCE_ONLY
 
@@ -121,6 +125,7 @@ enum netfs_sreq_trace { netfs_sreq_traces } __mode(byte);
 enum netfs_failure { netfs_failures } __mode(byte);
 enum netfs_rreq_ref_trace { netfs_rreq_ref_traces } __mode(byte);
 enum netfs_sreq_ref_trace { netfs_sreq_ref_traces } __mode(byte);
+enum netfs_region_trace { netfs_region_traces } __mode(byte);
 
 #endif
 
@@ -140,6 +145,7 @@ netfs_sreq_traces;
 netfs_failures;
 netfs_rreq_ref_traces;
 netfs_sreq_ref_traces;
+netfs_region_traces;
 
 /*
  * Now redefine the EM() and E_() macros to map the enums to the strings that
@@ -357,9 +363,12 @@ TRACE_EVENT(netfs_wback,
 
 	    TP_fast_assign(
 		    struct fscache_cookie *__cookie = netfs_i_cookie(wreq->inode);
+		    struct netfs_dirty_region *__region =
+		    list_first_entry_or_null((struct list_head *)&wreq->regions,
+					     struct netfs_dirty_region, dirty_link);
 		    __entry->wreq	= wreq->debug_id;
 		    __entry->cookie	= __cookie ? __cookie->debug_id : 0;
-		    __entry->region	= 0;
+		    __entry->region	= __region ? __region->debug_id : 0;
 		    __entry->start	= wreq->start;
 		    __entry->len	= wreq->len;
 		    __entry->first	= wreq->first;
@@ -372,6 +381,30 @@ TRACE_EVENT(netfs_wback,
 		      __entry->region,
 		      __entry->start, __entry->start + __entry->len - 1,
 		      __entry->first, __entry->last)
+	    );
+
+TRACE_EVENT(netfs_ref_region,
+	    TP_PROTO(unsigned int region_debug_id, int ref,
+		     enum netfs_region_trace what),
+
+	    TP_ARGS(region_debug_id, ref, what),
+
+	    TP_STRUCT__entry(
+		    __field(unsigned int,		region		)
+		    __field(int,			ref		)
+		    __field(enum netfs_region_trace,	what		)
+			     ),
+
+	    TP_fast_assign(
+		    __entry->region	= region_debug_id;
+		    __entry->ref	= ref;
+		    __entry->what	= what;
+			   ),
+
+	    TP_printk("D=%x %s r=%u",
+		      __entry->region,
+		      __print_symbolic(__entry->what, netfs_region_traces),
+		      __entry->ref)
 	    );
 
 #undef EM