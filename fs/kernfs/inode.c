7147c2ba2f1bd9703454acd1130985130c12a312
Jeff Layton <jlayton@kernel.org>
2023-06-20 08:27:22 -0400

    kernfs: switch to new ctime accessors
    In later patches, we're going to change how the ctime.tv_nsec field is
    utilized. Switch to using accessor functions instead of raw accesses of
    inode->i_ctime.
    
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Jeff Layton <jlayton@kernel.org>

--- fs/kernfs/inode.c
+++ fs/kernfs/inode.c
@@ -152,7 +152,7 @@ static inline void set_default_inode_attr(struct inode *inode, umode_t mode)
 {
 	inode->i_mode = mode;
 	inode->i_atime = inode->i_mtime =
-		inode->i_ctime = current_time(inode);
+		inode_ctime_set_current(inode);
 }
 
 static inline void set_inode_attr(struct inode *inode,
@@ -162,7 +162,7 @@ static inline void set_inode_attr(struct inode *inode,
 	inode->i_gid = attrs->ia_gid;
 	inode->i_atime = attrs->ia_atime;
 	inode->i_mtime = attrs->ia_mtime;
-	inode->i_ctime = attrs->ia_ctime;
+	inode_ctime_set(inode, attrs->ia_ctime);
 }
 
 static void kernfs_refresh_inode(struct kernfs_node *kn, struct inode *inode)
