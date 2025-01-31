22edf37a8668ea8f112e30c78cb041d90b45b425
Jeff Layton <jlayton@kernel.org>
2025-01-31 10:18:11 -0500

    fuse: add a new "connections" file to show longest waiting reqeust
    Add a new file to the "connections" directory that shows how long (in
    seconds) the oldest fuse_req in the processing hash or pending queue has
    been waiting.
    
    Reviewed-by: Joanne Koong <joannelkoong@gmail.com>
    Signed-off-by: Jeff Layton <jlayton@kernel.org>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -42,7 +42,7 @@
 #define FUSE_NAME_MAX 1024
 
 /** Number of dentries for each connection in the control filesystem */
-#define FUSE_CTL_NUM_DENTRIES 5
+#define FUSE_CTL_NUM_DENTRIES 6
 
 /* Frequency (in seconds) of request timeout checks, if opted into */
 #define FUSE_TIMEOUT_TIMER_FREQ 15
