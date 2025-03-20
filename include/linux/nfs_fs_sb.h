aa7df5a36800737963510005320b69cce8765cbb
Trond Myklebust <trond.myklebust@hammerspace.com>
2025-03-20 11:45:06 -0400

    NFS: Add a mount option to make ENETUNREACH errors fatal
    If the NFS client was initially created in a container, and that
    container is torn down, there is usually no possibity to go back and
    destroy any NFS clients that are hung because their virtual network
    devices have been unlinked.
    
    Add a flag that tells the NFS client that in these circumstances, it
    should treat ENETDOWN and ENETUNREACH errors as fatal to the NFS client.
    
    The option defaults to being on when the mount happens from inside a net
    namespace that is not "init_net".
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Tested-by: Jeff Layton <jlayton@kernel.org>
    Acked-by: Chuck Lever <chuck.lever@oracle.com>
    (cherry picked from commit 487fae09d7e266a58a13784983cc1ef6a2076526)

--- include/linux/nfs_fs_sb.h
+++ include/linux/nfs_fs_sb.h
@@ -168,6 +168,7 @@ struct nfs_server {
 #define NFS_MOUNT_TRUNK_DISCOVERY	0x04000000
 #define NFS_MOUNT_SHUTDOWN			0x08000000
 #define NFS_MOUNT_NO_ALIGNWRITE		0x10000000
+#define NFS_MOUNT_NETUNREACH_FATAL	0x40000000
 
 	unsigned int		fattr_valid;	/* Valid attributes */
 	unsigned int		caps;		/* server capabilities */
