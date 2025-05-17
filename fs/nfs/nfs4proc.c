17ddc757d855d2aeb03c258db91e907b66202d2d
Kyle McMartin <jkkm@jkkm.org>
2025-05-16 18:25:20 -0700

    Revert "NFS: Treat ENETUNREACH errors as fatal in containers"
    This reverts commit 71c8bf2a24105777349b6f5ecc80e1dd7087e3a4.

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -195,9 +195,6 @@ static int nfs4_map_errors(int err)
 		return -EBUSY;
 	case -NFS4ERR_NOT_SAME:
 		return -ENOTSYNC;
-	case -ENETDOWN:
-	case -ENETUNREACH:
-		break;
 	default:
 		dprintk("%s could not handle NFSv4 error %d\n",
 				__func__, -err);
