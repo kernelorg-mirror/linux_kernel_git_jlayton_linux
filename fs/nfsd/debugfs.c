198bc27df52244f0e8a420e7a40552c3ac45c905
Mike Snitzer <snitzer@kernel.org>
2025-06-16 19:08:09 -0400

    NFSD: add io_cache_write controls to debugfs interface
    Add 'io_cache_write' to NFSD's debugfs interface so that: Any data
    written by NFSD will either be:
    - cached using page cache (NFSD_IO_BUFFERED=0)
    - cached but removed from the page cache upon completion
      (NFSD_IO_DONTCACHE=1).
    - not cached (NFSD_IO_DIRECT=2)
    
    io_cache_write is 0 by default.  It may be set by writing to:
      /sys/kernel/debug/nfsd/io_cache_write
    
    If NFSD_IO_DONTCACHE is specified using 1, FOP_DONTCACHE must be
    advertised as supported by the underlying filesystem (e.g. XFS),
    otherwise all IO flagged with RWF_DONTCACHE will fail with
    -EOPNOTSUPP.
    
    If NFSD_IO_DIRECT is specified using 2, the IO must be aligned
    relative to the underlying block device's logical_block_size. Also the
    memory buffer used to store the WRITE payload must be aligned relative
    to the underlying block device's dma_alignment.
    
    Signed-off-by: Mike Snitzer <snitzer@kernel.org>

--- fs/nfsd/debugfs.c
+++ fs/nfsd/debugfs.c
@@ -82,6 +82,44 @@ static int nfsd_io_cache_read_set(void *data, u64 val)
 DEFINE_DEBUGFS_ATTRIBUTE(nfsd_io_cache_read_fops, nfsd_io_cache_read_get,
 			 nfsd_io_cache_read_set, "%llu\n");
 
+/*
+ * /sys/kernel/debug/nfsd/io_cache_write
+ *
+ * Contents:
+ *   %0: NFS WRITE will use buffered IO (default)
+ *   %1: NFS WRITE will use dontcache (buffered IO w/ dropbehind)
+ *   %2: NFS WRITE will use direct IO
+ *
+ * The default value of this setting is zero (buffered IO is
+ * used). This setting takes immediate effect for all NFS
+ * versions, all exports, and in all NFSD net namespaces.
+ */
+
+static int nfsd_io_cache_write_get(void *data, u64 *val)
+{
+	*val = nfsd_io_cache_write;
+	return 0;
+}
+
+static int nfsd_io_cache_write_set(void *data, u64 val)
+{
+	switch (val) {
+	case NFSD_IO_BUFFERED:
+	case NFSD_IO_DONTCACHE:
+	case NFSD_IO_DIRECT:
+		nfsd_io_cache_write = val;
+		break;
+	default:
+		nfsd_io_cache_write = NFSD_IO_BUFFERED;
+		break;
+	}
+
+	return 0;
+}
+
+DEFINE_DEBUGFS_ATTRIBUTE(nfsd_io_cache_write_fops, nfsd_io_cache_write_get,
+			 nfsd_io_cache_write_set, "%llu\n");
+
 void nfsd_debugfs_exit(void)
 {
 	debugfs_remove_recursive(nfsd_top_dir);
@@ -97,4 +135,7 @@ void nfsd_debugfs_init(void)
 
 	debugfs_create_file("io_cache_read", S_IWUSR | S_IRUGO,
 			    nfsd_top_dir, NULL, &nfsd_io_cache_read_fops);
+
+	debugfs_create_file("io_cache_write", S_IWUSR | S_IRUGO,
+			    nfsd_top_dir, NULL, &nfsd_io_cache_write_fops);
 }
--- fs/nfsd/vfs.c
+++ fs/nfsd/vfs.c
@@ -50,6 +50,7 @@
 
 bool nfsd_disable_splice_read __read_mostly;
 u64 nfsd_io_cache_read __read_mostly;
+u64 nfsd_io_cache_write __read_mostly;
 
 /**
  * nfserrno - Map Linux errnos to NFS errnos
@@ -1229,6 +1230,20 @@ nfsd_vfs_write(struct svc_rqst *rqstp, struct svc_fh *fhp,
 
 	nvecs = xdr_buf_to_bvec(rqstp->rq_bvec, rqstp->rq_maxpages, payload);
 	iov_iter_bvec(&iter, ITER_SOURCE, rqstp->rq_bvec, nvecs, *cnt);
+
+	switch (nfsd_io_cache_write) {
+	case NFSD_IO_DIRECT:
+		if (iov_iter_is_aligned(&iter, nf->nf_dio_mem_align - 1,
+					nf->nf_dio_offset_align - 1))
+			kiocb.ki_flags = IOCB_DIRECT;
+		break;
+	case NFSD_IO_DONTCACHE:
+		kiocb.ki_flags = IOCB_DONTCACHE;
+		break;
+	case NFSD_IO_BUFFERED:
+		break;
+	}
+
 	since = READ_ONCE(file->f_wb_err);
 	if (verf)
 		nfsd_copy_write_verifier(verf, nn);
